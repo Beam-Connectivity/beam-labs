5fef6cb23a1dc3b761aead853cdf5b9295311ab3
Rob Potter <rob@beamconnectivity.com>
2022-11-02 11:08:57 +0000

    Added vcan blog post scripts

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# tools
-Tools and scripts that we use in software development
+# Beam Connectivity: Labs
+
+This repo is a storage ground for small tools and scripts that we want to make public.
+
+We value any feedback or improvement on these - feel free to raise issues, PRs, etc.
+
+
+## Contents
+
+| tool | desc | link
+| --- | --- | ---
+| vcan-in-wsl2 | Script to re-build the WSL2 linux kernel to include the vcan module. | [Blog post](https://www.beamconnectivity.com/blog/software-dev-can-bus-in-windows-11)
+
+
+
+## License
+
+These tools are MIT licensed.
+
+- - - 
+
+Have fun and stay safe.
+
+The Beam Team
+
+🌍 https://beamconnectivity.com
+
+🧑🏼‍💻 https://dev.beamconnectivity.com
+
+🐦 https://twitter.com/we_are_beam
---
+++ vcan-in-wsl2/vcan-in-wsl2.sh
@@ -0,0 +1,35 @@
+# required packages
+sudo apt install -y libelf-dev dwarves
+ 
+# The below few connamds will checkout the 1GB default kernel
+git clone https://github.com/microsoft/WSL2-Linux-Kernel
+cd WSL2-Linux-Kernel/
+git checkout linux-msft-wsl-`uname -r | cut -d'-' -f1 |  tr -d '\n'`
+cat /proc/config.gz | gunzip > .config
+make prepare modules_prepare
+ 
+# The next command brings up the kernel configuration.
+# In here we can select vcan and slcan. You can also enable other modules you
+# require and test if they work. The link below helps locate modules in the
+# menu config display. For wsl v5.10.102 it was located at:
+# Networking support -> CAN bus subsystem support -> CaN Device Drivers -> vcan
+# https://www.kernelconfig.io/config_can_vcan
+make menuconfig 
+
+#build the kernel
+make -j4
+
+# We'll need to store our custom kernel in the Windows environment and 
+# launch it when we start wsl2.
+# Change the path below to where you want to store it
+cp vmlinux /mnt/c/Users/<USER>/
+cat >> /mnt/c/Users/<USER>/.wslconfig << ENDL
+echo kernel=C:\\Users\\<USER>\\vmlinux
+ENDL
+
+# We'll need to restart WSL to use the new kernel
+# In the command prompt enter the next couple of commands
+wsl --shutdown
+# Wait 10 seconds or check everything is stopped with
+wsl --list -v
+
