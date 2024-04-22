42263a046b56723afc999d8a5291e88c48e64193
Rob Geografo <rob.geografo@beamconnectivity.com>
2024-04-22 11:22:32 +0100

    Updated steps to use working versions

--- vcan-in-wsl2/vcan-in-wsl2.sh
+++ vcan-in-wsl2/vcan-in-wsl2.sh
@@ -1,11 +1,14 @@
 # required packages
-sudo apt install -y libelf-dev dwarves flex bison libssl-dev \
-    libncurses-dev
- 
+sudo apt install -y libelf-dev flex bison libssl-dev libncurses-dev bc
+
+# The v1.21 of dwarves (and its dependency pahole) is required. Download it in your preferred directory and install it
+wget http://archive.ubuntu.com/ubuntu/pool/universe/d/dwarves-dfsg/dwarves_1.21-0ubuntu1~20.04.1_amd64.deb
+sudo apt install ./dwarves_1.21-0ubuntu1~20.04.1_amd64.deb
+
 # The below few connamds will checkout the 1GB default kernel
-git clone https://github.com/microsoft/WSL2-Linux-Kernel
+git clone https://github.com/microsoft/WSL2-Linux-Kernel --shallow-since=2022-07-30
 cd WSL2-Linux-Kernel/
-git checkout linux-msft-wsl-`uname -r | cut -d'-' -f1 |  tr -d '\n'`
+git checkout linux-msft-wsl-5.15.57.1 # This version has canfd drivers as well as regular can drivers
 cat /proc/config.gz | gunzip > .config
 make prepare modules_prepare
  
@@ -17,10 +20,10 @@ make prepare modules_prepare
 # https://www.kernelconfig.io/config_can_vcan
 make menuconfig 
 
-#build the kernel
-make -j4
-sudo make module_install
-sudo ln -s /lib/modules/5.10.16.3-microsoft-standard-WSL2+/ /lib/modules/5.10.16.3-microsoft-standard-WSL2
+# build the kernel
+make -j$(nproc)
+sudo make modules_install
+sudo ln -s /lib/modules/5.15.57.1-microsoft-standard-WSL2+/ /lib/modules/5.15.57.1-microsoft-standard-WSL2
 # We'll need to store our custom kernel in the Windows environment and 
 # launch it when we start wsl2.
 # Change the path below to where you want to store it
@@ -36,3 +39,5 @@ ENDL
 # Wait 10 seconds or check everything is stopped with
 # wsl --list -v
 
+## Check new custom kernel is used
+uname -a
