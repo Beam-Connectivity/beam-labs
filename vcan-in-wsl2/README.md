6e8c25a00be5ba66ac8fe8314a205589ce76e060
RobertoGeografo <rob.geografo@outlook.com>
2024-04-23 23:17:55 +0100

    Tested on fresh machine failed

--- vcan-in-wsl2/README.md
+++ vcan-in-wsl2/README.md
@@ -3,3 +3,13 @@
 In Windows, open a WSL terminal and launch `vcan-in-wsl2.sh` script.
 
 A Linux Kernel Configuration menu will open. In the `General Setup` section, we can select vcan and slcan (press M for module support)
+
+Go to Networking support
+M CAN bus subsystem support
+M ISO 15765-2:2016 CAN transport protocol
+
+Go to CAN Device Drivers
+M Virtual Local CAN Interface (vcan)
+M Serial / USB serial CAN Adaptors (slcan)
+M Kvaser PCIe FD Cards
+M PEAK-System PCAN-PCIe FD cards
--- vcan-in-wsl2/vcan-in-wsl2.sh
+++ vcan-in-wsl2/vcan-in-wsl2.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-SCRIPT_PATH="$(dirname "$0")"
+# #!/bin/bash
+
+# Get the directory of the script
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update and install required packages
 echo !!!!!!!!!!!!!!!!!!!!!!!!!
@@ -7,13 +9,17 @@ echo Install required packages
 echo !!!!!!!!!!!!!!!!!!!!!!!!!
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y libelf-dev flex bison libssl-dev libncurses-dev bc build-essential make
+sudo apt install -y --no-install-recommends wslu
 sleep 5
 
 # The v1.21 of dwarves (and its dependency pahole) is required.
 echo !!!!!!!!!!!!!!!!!!!!!
 echo Install dwarves v1.21
 echo !!!!!!!!!!!!!!!!!!!!!
+if [[ ! -f "dwarves_1.21-0ubuntu1~20.04.1_amd64.deb" ]]; then
+echo !!!!!!!!!!!!!!Downloading dwarves 1.21!!!!!!!!!!!!!!!!!!!!!
 wget http://archive.ubuntu.com/ubuntu/pool/universe/d/dwarves-dfsg/dwarves_1.21-0ubuntu1~20.04.1_amd64.deb
+fi
 sudo apt install ./dwarves_1.21-0ubuntu1~20.04.1_amd64.deb
 sleep 5
 
@@ -22,7 +28,7 @@ echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo Clone and checkout the WSL2 Linux Kernel
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 if [[ ! -d "/WSL2-Linux-Kernel/" ]]; then
-    git clone https://github.com/microsoft/WSL2-Linux-Kernel --shallow-since=2022-07-30
+    git clone git@github.com:microsoft/WSL2-Linux-Kernel.git --shallow-since=2022-07-30
 fi
 cd WSL2-Linux-Kernel/
 git checkout linux-msft-wsl-5.15.57.1 # This version has canfd drivers as well as regular can drivers
@@ -41,24 +47,26 @@ sleep 5
 echo !!!!!!!!!!!!
 echo Build kernel
 echo !!!!!!!!!!!!
-make -j$(nproc)
+make -j$(( $(nproc) - 1 ))
 sudo make modules_install
 sudo ln -s /lib/modules/5.15.57.1-microsoft-standard-WSL2+/ /lib/modules/5.15.57.1-microsoft-standard-WSL2
 sleep 5
 
 # Find and store Windows username
-WIN_USER=$(powershell.exe '$env:UserName')
+WIN_USER=$(wslpath "$(wslvar USERPROFILE)")
 
 # Copy kernel to Windows home directory
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo Copy kernel and wsl-reboot.sh to Windows
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-cp vmlinux /mnt/c/Users/$WIN_USER/
-cat >> /mnt/c/Users/$WIN_USER/.wslconfig << "ENDL"
+cp vmlinux $WIN_USER/
+cd $WIN_USER
+touch .wslconfig
+cat >> .wslconfig << "ENDL"
 [wsl2]
 kernel=C:\\Users\\$WIN_USER\\vmlinux
 ENDL
 cd $SCRIPT_PATH
-cp wsl-reboot.ps1 /mnt/c/Users/$WIN_USER/
-
+echo $SCRIPT_PATH
+cp wsl-reboot.ps1 $WIN_USER
 echo Done! Now open a Windows powershell and launch the wsl-reboot.ps1 script to reboot WSL.
